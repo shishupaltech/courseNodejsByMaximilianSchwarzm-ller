1366bbc561d21ac552f31b75cd044e9de58b8b1a
shishupaltech <veerutech75@gmail.com>
2023-02-20 16:52:09 +0530

    how the web works file added

---
+++ section3_understanding_the_basic/25_how_the_web_works.js
@@ -0,0 +1,13 @@
+// How the Web Works 
+
+// user/ client (Broser)<-------------------------------
+//  |       // |                                        |
+//  |       // enter                                    |
+//  |       // http://my-page.com                       |
+//  |       // |                                        |
+// Request--->// Domain Lookup                        Response(e.g HTML-page)
+// |                                                  |
+// |                                                  |
+// |----------> Server at(10.212.212.12)---------------
+//                  |
+// nodejs,php,asp.net <your code><-----------------Databaseg
