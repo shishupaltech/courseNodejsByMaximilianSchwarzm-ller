4e779d977a116df70114a01676f268db3269d40a
shishupaltech <veerutech75@gmail.com>
2023-02-25 11:30:43 +0530

    section 6 is completed

--- section6_working_with_dynamic_content&adding_template_engine/85_adding_layout.js
+++ section6_working_with_dynamic_content&adding_template_engine/85_adding_layout.js
@@ -4,8 +4,17 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const app = express();
+const expressHbs = require('express-handlebars');
 
-app.set('view engine','pug');
+app.engine(
+    'hbs',
+    expressHbs({
+        layoutsDir:'views/layouts/',
+        defaultLayout:'main-layout',
+        extname:'hbs'
+    })
+);
+app.set('view engine','hbs');
 app.set('views','views');
 
 // const adminRoutes = require('./routes/admin');
--- section6_working_with_dynamic_content&adding_template_engine/routes/shop.js
+++ section6_working_with_dynamic_content&adding_template_engine/routes/shop.js
@@ -3,16 +3,20 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
-
-const router = express.Router();
 const adminData = require('./admin');
 
+const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    console.log(adminData.products);
-    const products = adminData.products;
-  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
-  res.render('shop',{prods:products,docTitle:'Shop'});
+  const products = adminData.products;
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+    activeShop: true,
+    productCSS: true
+  });
 });
 
 module.exports = router;
