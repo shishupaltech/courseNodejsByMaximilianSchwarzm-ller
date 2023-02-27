279994a1df373368db71d68ed904e6ac050f1093
shishupaltech <veerutech75@gmail.com>
2023-02-27 20:30:13 +0530

    Adding an Order Model is added

--- section11_understanding_sequelize/controllers/shop.js
+++ section11_understanding_sequelize/controllers/shop.js
@@ -87,18 +87,55 @@ exports.getCart = (req, res, next) => {
 };
 
 exports.postCart = (req, res, next) => {
+  // const prodId = req.body.productId;
+  // Product.findByPk(prodId, product => {
+  //   Cart.addProduct(prodId, product.price);
+  // });
+  // res.redirect('/cart');
   const prodId = req.body.productId;
-  Product.findByPk(prodId, product => {
-    Cart.addProduct(prodId, product.price);
-  });
-  res.redirect('/cart');
+  let fetchedCart;
+  let newQuantity=1;
+  req.user.getCart().then(cart=>{
+    fetchedCart = cart;
+    return cart.getProducts({where:{id:prodId}});
+  }).then(products=>{
+    let product;
+    if(products.length>0){
+      product=products[0];
+    }
+    
+    if(product){
+      const oldQuantity = productItem.quantity;
+      newQuantity = oldQuantity+1;
+      return product;
+      
+    }
+    return Product.findByPk(prodId);
+  }).then(product=>{
+    return fetchedCart.addProduct(product,{through:{quantity:newQuantity}});
+  }).
+    then(()=>{
+    res.redirect('/cart');
+  })
+  .catch(err=>console.log(err));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
+
+  req.user.getCart().then(cart=>{
+    return cart.getProduct({where:{id:prodId}});
+  })
   Product.findByPk(prodId, product => {
     Cart.deleteProduct(prodId, product.price);
     res.redirect('/cart');
+  }).then(products=>{
+    const product = products[0];
+    product.cartItem.destroy();
+  }).then(result=>{
+    res.redirect("/cart");
+  }).catch(err=>{
+    console.log(err);
   });
 };
 
