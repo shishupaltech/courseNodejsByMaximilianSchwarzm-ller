e8c0313323b6d1320fa5effaf0ec2cd52add4a82
shishupaltech <veerutech75@gmail.com>
2023-03-15 17:21:17 +0530

    Adding the request Driven Login Solution

---
+++ section14_Session_and_Cookies/controllers/auth.js
@@ -0,0 +1,15 @@
+exports.getLogin = (req, res, next) => {
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated:req.isAuthenticated
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+
+    req.isLoggedIn = true;
+
+    res.redirect('/');
+  };
+  
