ad8d93c82ebbfc0c5e1b5a4f3ea60dd46bac1574
shishupaltech <veerutech75@gmail.com>
2023-03-16 08:24:48 +0530

    section14 is completed

--- section14_Session_and_Cookies/app.js
+++ section14_Session_and_Cookies/app.js
@@ -36,6 +36,16 @@ app.use(
   })
 );
 
+app.use((req,res,next)=>{
+  User.findById(
+    req.session.user._id
+    )
+  .then(user => {
+    req.user = user;
+    next();
+  })
+  .catch(err => console.log(err));
+})
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 app.use(authRoutes);
