3ae32184e769906d0509edb6f3b74bb3c81c0963
shishupaltech <veerutech75@gmail.com>
2023-03-16 12:33:08 +0530

    Working on Route Protection is done

--- section15_Adding_Authentication/app.js
+++ section15_Adding_Authentication/app.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const mongoose = require('mongoose');   
+const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 
